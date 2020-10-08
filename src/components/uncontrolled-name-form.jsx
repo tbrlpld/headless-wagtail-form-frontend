8863d422a83bd73f608652affb7f9ef435fe2a47
Tibor Leupold <tibor@lpld.io>
2020-10-08 14:41:01 -0700

    Add simple validation to uncontrolled input
    This is just a proof of concept, that it is possible to create immediate
    validation feedback to the use while still using an uncontrolled component.

--- src/components/uncontrolled-name-form.jsx
+++ src/components/uncontrolled-name-form.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
+import style from './uncontrolled-name-form.modules.css'
+
 export default class UncontrolledNameForm extends React.Component {
   constructor (props) {
     super(props)
     this.input = React.createRef()
     this.handleSubmit = this.handleSubmit.bind(this)
+    this.validateInput = this.validateInput.bind(this)
   }
 
   handleSubmit (event) {
@@ -12,12 +15,24 @@ export default class UncontrolledNameForm extends React.Component {
     alert('A name was submitted: ' + this.input.current.value)
   }
 
+  validateInput (event) {
+    const target = event.target
+    const value = this.input.current.value
+    if (value.includes('Doe')) {
+      target.classList.add(style.valid)
+      target.classList.remove(style.invalid)
+    } else {
+      target.classList.remove(style.valid)
+      target.classList.add(style.invalid)
+    }
+  }
+
   render () {
     return (
       <form onSubmit={this.handleSubmit}>
         <label htmlFor='name'>
-          Name:
-          <input type='text' ref={this.input} />
+          Name (include Doe):
+          <input type='text' value='' ref={this.input} onChange={this.validateInput} />
         </label>
         <input type='submit' value='Submit' />
       </form>

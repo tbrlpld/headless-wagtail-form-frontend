a91b67643c01fb35251caabf3716cd414da00c1b
Tibor Leupold <tibor@lpld.io>
2020-10-15 14:54:37 -0700

    Link to other wagtail pages from Home

--- src/templates/home.js
+++ src/templates/home.js
@@ -1,15 +1,33 @@
 import React from 'react'
 import { Link, graphql } from 'gatsby'
 
-export default function () {
+export default function ({ data }) {
+  const wagtailPages = data.wagtail.pages.filter(item => item.contentType === 'content.SomePage')
+  const wagtailPageLinks = wagtailPages.map((item) => <Link key={item.id} to={item.url}>{item.title}</Link>)
   return (
     <>
-      <h1>Hello to the world!</h1>
+      <h1>{data.wagtail.page.title}</h1>
+      <h2>Wagtail Pages</h2>
+      {wagtailPageLinks}
+      <h2>Other Pages</h2>
       <Link to='react-forms'>React Forms</Link>
     </>
   )
 }
 
-// export const query = graphql`
-
-// `
+export const query = graphql`
+  query ($slug: String) {
+    wagtail {
+      page(slug: $slug) {
+        id
+        title
+      }
+      pages {
+        id
+        title
+        url
+        contentType
+      }
+    }
+  }
+`

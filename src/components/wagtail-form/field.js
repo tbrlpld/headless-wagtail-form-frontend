d53dc5089b91b36377c1a11323706d846538aee5
Tibor Leupold <tibor@lpld.io>
2020-10-16 10:31:12 -0700

    Add case for hidden field type

--- src/components/wagtail-form/field.js
+++ src/components/wagtail-form/field.js
@@ -20,6 +20,18 @@ const Field = React.forwardRef((props, ref) => {
         />
       )
       break
+    case 'hidden':
+      fieldElement = (
+        <input
+          type='hidden'
+          ref={ref}
+          defaultValue={props.defaultValue}
+          name={props.cleanName}
+          required={props.required}
+          placeholder={props.helpText}
+        />
+      )
+      break
     default:
       fieldElement = (
         <input
@@ -31,9 +43,12 @@ const Field = React.forwardRef((props, ref) => {
         />
       )
   }
+  const label = (props.label && props.fieldType.toLowerCase() !== 'hidden')
+    ? (<label htmlFor={props.cleanName}>{props.label}</label>)
+    : null
   return (
     <>
-      <label htmlFor={props.cleanName}>{props.label}</label>
+      {label}
       {fieldElement}
     </>
   )
